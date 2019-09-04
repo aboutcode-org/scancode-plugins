3a2f18cd7c5382ef62ed273a39b04779b4d5e303
Li <li@nexb.com>
2019-09-04 21:41:46 +0800

    Rename the plulgin name #1712
    Signed-off-by: Li <li@nexb.com>

--- scancode-compiledcode/setup.py
+++ scancode-compiledcode/setup.py
@@ -13,17 +13,17 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-desc = '''A ScanCode scan plugin to get Linux lkmclue and elf info.'''
+desc = '''A ScanCode scan plugin to get Linux lkmclue, cpp includes and elf info.'''
 
 setup(
-    name='scancode-lkmclue',
+    name='scancode-compiledcode',
     version='1.0.0',
     license='Apache-2.0 with ScanCode acknowledgment',
     description=desc,
     long_description=desc,
     author='nexB',
     author_email='info@aboutcode.org',
-    url='https://github.com/nexB/scancode-toolkit/plugins/scancode-lkmclue',
+    url='https://github.com/nexB/scancode-toolkit/plugins/scancode-compiledcode',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
