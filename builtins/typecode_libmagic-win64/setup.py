797c6ffcfb97bee06cbc2f1134d65f77ac334aa8
Philippe Ombredanne <pombredanne@nexb.com>
2020-05-20 18:14:58 +0200

    Bump to latest libmagic 5.38
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- builtins/typecode_libmagic-win64/setup.py
+++ builtins/typecode_libmagic-win64/setup.py
@@ -17,7 +17,7 @@ desc = '''A ScanCode path provider plugin to provide a prebuilt native libmagic 
 
 setup(
     name='typecode_libmagic',
-    version='5.37.1.1',
+    version='5.38.1.1',
     license='bsd-simplified AND other-permissive AND other-copyleft',
     description=desc,
     long_description=desc,
