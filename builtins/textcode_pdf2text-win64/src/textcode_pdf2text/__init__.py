052a410a866c04ac996774741697e5ccddbe48e7
Philippe Ombredanne <pombredanne@nexb.com>
2021-02-16 09:05:28 +0100

    Add new plugins skeletton for pdf2text
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ builtins/textcode_pdf2text-win64/src/textcode_pdf2text/__init__.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) nexB Inc. and others.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Visit https://aboutcode.org and https://github.com/nexB/ for support and download.
+# ScanCode is a trademark of nexB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+
+from os.path import abspath
+from os.path import dirname
+from os.path import join
+
+from plugincode.location_provider import LocationProviderPlugin
+
+
+class Pdf2textPaths(LocationProviderPlugin):
+    def get_locations(self):
+        curr_dir = dirname(abspath(__file__))
+        bin_dir = join(curr_dir, 'bin')
+        locations = {
+            'textcode.pdf2text.bindir': bin_dir,
+            'textcode.pdf2text.exe': join(bin_dir, 'pdf2text.exe'),
+        }
+        return locations
